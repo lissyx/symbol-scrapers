4d59eb4e367f9d5071bbaee1802511e73f8996ea
Alexandre Lissy <lissyx@lissyx.dyndns.org>
2025-02-12 19:37:05 +0100

    test

--- thunderbird-snap/script.sh
+++ thunderbird-snap/script.sh
@@ -5,9 +5,12 @@ export DEBUGINFOD_URLS="https://debuginfod.ubuntu.com/"
 . $(dirname $0)/../common.sh
 . $(dirname $0)/../launchpad.sh
 
-process_snap "thunderbird" "thunderbird" "desktop-snappers"
-process_snap "thunderbird-stable-core24" "thunderbird" "desktop-snappers"
-process_snap "thunderbird-beta" "thunderbird" "desktop-snappers"
+#process_snap "thunderbird" "thunderbird" "desktop-snappers"
+#process_snap "thunderbird-stable-core24" "thunderbird" "desktop-snappers"
+#process_snap "thunderbird-beta" "thunderbird" "desktop-snappers"
+#process_snap "thunderbird-beta-core24" "thunderbird" "desktop-snappers"
+
+process_snap "tbird-beta-debugsyms" "thunderbird" "hellsworth1"
 
 create_symbols_archive
 
